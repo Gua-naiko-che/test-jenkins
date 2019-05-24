15788e2a4c329a2dff57e626dd3d4ca834be8897
Tao Gómez Gil <tgomezgil@talentsoft.com>
2019-05-24 15:44:39 +0200

    make it work with docker-compose

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -12,21 +12,19 @@ namespace tests
         [Fact]
         public void Test1()
         {
-            Assert.True(true);
-            return;
             // Puede que sea por ser una URL externa
             var options = new ChromeOptions();
             options.AddArgument("--headless");
             options.AddArgument("--no-sandbox");
             options.AddArgument("--disable-dev-shm-usage");
             options.BinaryLocation = "/opt/google/chrome/chrome";
-            ChromeDriverService service = ChromeDriverService.CreateDefaultService(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "chromedriver.exe");
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "chromedriver");
             using (var driver = new ChromeDriver(service, options))
             {
-                driver.Navigate().GoToUrl("http://localhost:1234/");
+                driver.Navigate().GoToUrl("http://webapp/");
                 var welcomeTitleSelector = By.ClassName("display-4");
                 var welcomeTitle = driver.FindElement(welcomeTitleSelector);
-                Assert.Equal("Welcome", welcomeTitle.Text);
+                Assert.Equal("WelcomeX", welcomeTitle.Text);
             }
         }
     }
