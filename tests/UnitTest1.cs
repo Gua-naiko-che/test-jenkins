1d300782b4c65e74fb9236a2978032c79d8a0129
Tao Gómez Gil <tgomezgil@talentsoft.com>
2019-05-24 16:31:44 +0200

    Cambiar donde esta el driver

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -18,13 +18,13 @@ namespace tests
             options.AddArgument("--no-sandbox");
             options.AddArgument("--disable-dev-shm-usage");
             options.BinaryLocation = "/opt/google/chrome/chrome";
-            ChromeDriverService service = ChromeDriverService.CreateDefaultService(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "chromedriver");
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService("/opt/selenium", "chromedriver");
             using (var driver = new ChromeDriver(service, options))
             {
                 driver.Navigate().GoToUrl("http://webapp/");
                 var welcomeTitleSelector = By.ClassName("display-4");
                 var welcomeTitle = driver.FindElement(welcomeTitleSelector);
-                Assert.Equal("WelcomeX", welcomeTitle.Text);
+                Assert.Equal("Welcome", welcomeTitle.Text);
             }
         }
     }
